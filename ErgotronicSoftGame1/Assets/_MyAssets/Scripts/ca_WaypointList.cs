a46f91c9acf2f2bfeae5dcd6bb486beb3023c79c
CarloAlbino <carloalbino@gmail.com>
2016-03-24 12:44:20 -0400

    Enemy Patrol
    Enemy patrols, enemy detecting is incoming

---
+++ ErgotronicSoftGame1/Assets/_MyAssets/Scripts/ca_WaypointList.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ca_WaypointList : MonoBehaviour {
+
+    public Transform[] waypoints;
+    private Transform playerTarget;
+    private int nextTarget = 0;
+    private NavMeshAgent agent;
+    private bool canPatrol = true;
+
+	// Use this for initialization
+	void Start () {
+        agent = GetComponent<NavMeshAgent>();
+        UpdateTarget(0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (canPatrol)
+        {
+            // Patrol Waypoints
+            CheckArrive();
+        }
+        else
+        {
+            // Chase Player
+            ChasePlayer();
+        }
+	}
+
+    public void SetPartol(bool b)
+    {
+        if (b)
+        {
+            UpdateTarget(nextTarget);
+        }
+        canPatrol = b;
+    }
+
+    public void SetPlayerTarget(Transform player)
+    {
+        playerTarget = player;
+    }
+
+    private void ChasePlayer()
+    {
+        agent.destination = playerTarget.position;
+    }
+
+    private void UpdateTarget(int targetNum)
+    {
+        nextTarget = targetNum;
+        agent.destination = waypoints[nextTarget].position;
+    }
+
+    private void CheckArrive()
+    {
+        if((waypoints[nextTarget].position - agent.transform.position).magnitude <= 1f)
+        {
+            nextTarget++;
+            if (nextTarget >= waypoints.Length)
+                nextTarget = 0;
+            UpdateTarget(nextTarget);
+        }
+    }
+}
