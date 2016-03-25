43ff5b7df7888d977b1470d712d28ffe88d33f2f
Derek M <derekncmallory@gmail.com>
2016-03-25 18:17:17 -0400

    Player will be able to stand on a safe zone and hold F key to hide.

---
+++ ErgotronicSoftGame1/Assets/_MyAssets/Scripts/player/dm_SafeZone.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class dm_SafeZone : MonoBehaviour {
+    bool IsHiding = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    Debug.Log(IsHiding);
+
+        if (Input.GetKey(KeyCode.A))
+            transform.Translate(Vector3.back*Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.D))
+            transform.Translate(Vector3.forward*Time.deltaTime);
+	}
+
+    public bool Hiding
+    {
+        get
+        {
+            return IsHiding;
+        }
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("SafeZone"))
+        {
+            if (Input.GetKey(KeyCode.F) && !IsHiding)
+            {
+                IsHiding = true;
+
+                // TRIGGER ANIMATION
+            }
+            else if (!Input.GetKey(KeyCode.F) && IsHiding)
+            {
+                IsHiding = false;
+
+                // TRIGGER ANIMATION
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("SafeZone"))
+        {
+            IsHiding = false;
+
+            // TRIGGER ANIMATION
+        }
+    }
+}
