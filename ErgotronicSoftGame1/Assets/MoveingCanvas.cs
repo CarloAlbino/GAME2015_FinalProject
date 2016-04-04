564ebe578c6f5eab46d9f27b7255a3bf6d676840
Mohamud <detectivemomo1@gmail.com>
2016-04-04 12:17:53 -0400

    add gui
    Carlo   was a boss

---
+++ ErgotronicSoftGame1/Assets/MoveingCanvas.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveingCanvas : MonoBehaviour {
+    public GameObject moveinput;  
+    public float time;
+    public float speed;
+    bool CanSwicth = false;
+    IEnumerator changeDirection()
+    {
+       yield return new WaitForSeconds(time);
+        CanSwicth = !CanSwicth;
+        StartCoroutine(changeDirection());
+    }
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(changeDirection());
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (moveinput.GetComponent<Rigidbody>().velocity.magnitude > 00.1f)
+        {
+
+            if (CanSwicth == true)
+            {
+                transform.Translate(Vector3.up * speed * Time.deltaTime);
+            }
+            else
+            {
+                transform.Translate(-Vector3.up * speed * Time.deltaTime);
+            }
+        }
+	}
+}
